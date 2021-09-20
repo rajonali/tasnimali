a16fbea669fac1e02add7ccdd820628e6718781a
rajonali <rajonali@users.noreply.github.com>
2021-09-20 12:25:13 +0000

    Deploying to gh-pages from  @ 209c71bbb50dc2fce54da7103fd0a0ef210cfa08 🚀

---
+++ precache-manifest.847c98fc0039533ec64324e89998dd19.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6108e3517a587efafe6a871941681d7",
+    "url": "/tasnimali/index.html"
+  },
+  {
+    "revision": "818f0567fd9ccd3ba548",
+    "url": "/tasnimali/static/css/3.2c637b80.chunk.css"
+  },
+  {
+    "revision": "ad22aa6fbbf052d44482",
+    "url": "/tasnimali/static/css/4.96e7384e.chunk.css"
+  },
+  {
+    "revision": "a3d981a79d0fcc522228",
+    "url": "/tasnimali/static/css/main.5b7d6eda.chunk.css"
+  },
+  {
+    "revision": "27f98c955524c6de478c",
+    "url": "/tasnimali/static/js/2.4db383b7.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/tasnimali/static/js/2.4db383b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "818f0567fd9ccd3ba548",
+    "url": "/tasnimali/static/js/3.d1c78e36.chunk.js"
+  },
+  {
+    "revision": "ad22aa6fbbf052d44482",
+    "url": "/tasnimali/static/js/4.8b524665.chunk.js"
+  },
+  {
+    "revision": "a3d981a79d0fcc522228",
+    "url": "/tasnimali/static/js/main.1116f12a.chunk.js"
+  },
+  {
+    "revision": "921996cf62e7af288d6a",
+    "url": "/tasnimali/static/js/runtime-main.0b1d2523.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/tasnimali/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "0e746a48fa008ec31364c6dc59eb7c22",
+    "url": "/tasnimali/static/media/ListedB.0e746a48.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/tasnimali/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/tasnimali/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "4b2713c949069bc406dc02600e475c38",
+    "url": "/tasnimali/static/media/coursera.4b2713c9.jpg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/tasnimali/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "b618e42951a5f66cc057b321952cf31b",
+    "url": "/tasnimali/static/media/ems.b618e429.png"
+  },
+  {
+    "revision": "3e26049d9396f49912688a986b4027b8",
+    "url": "/tasnimali/static/media/googlelogo.3e26049d.png"
+  },
+  {
+    "revision": "2eadf21759b3d2d89cb480e5c887a2bc",
+    "url": "/tasnimali/static/media/lhsra.2eadf217.jpeg"
+  },
+  {
+    "revision": "ab34ca6a8a93e106624d258682134667",
+    "url": "/tasnimali/static/media/listedblogo.ab34ca6a.svg"
+  },
+  {
+    "revision": "32be7b6857b114da51870fe8919bf7ba",
+    "url": "/tasnimali/static/media/myavatar.32be7b68.png"
+  },
+  {
+    "revision": "46311ba3837a7ee0e92cc35d97b2fbba",
+    "url": "/tasnimali/static/media/nextuLogo.46311ba3.webp"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/tasnimali/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/tasnimali/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "551d895cca58ff567b47d51a69f5b0df",
+    "url": "/tasnimali/static/media/subrlogo.551d895c.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/tasnimali/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
